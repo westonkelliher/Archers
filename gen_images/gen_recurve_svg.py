43016435b1e1fdded3096dcdccc696cfc3df2266
Weston <westonkelliher@gmail.com>
2024-03-06 17:30:15 -0800

    script to generate recurve style bows

--- gen_images/gen_recurve_svg.py
+++ gen_images/gen_recurve_svg.py
@@ -20,15 +20,17 @@ def move_vector(v1, degrees, dist):
     return new_vector
 
 
+# Note: 0 angles mean straight up and down, positive angles mean outward and negative angle mean inward
 class RecurveDef:
-    def __init__(self, wing_len, wing_angle, curve_len, curve_angle,
-                 attach_len, attach_angle, handle_len=40, handle_dist=100, thickness=30,
-                 height=400, push=0, pull=0):
+    def __init__(self, wing_len, wing_angle, bend_mag, bend_angle,
+                 attach_depth, attach_mag, attach_angle, handle_len=30,
+                 handle_dist=100, thickness=30, height=400, push=0, pull=0):
         self.wing_len = wing_len
         self.wing_angle = wing_angle
-        self.curve_len = curve_len
-        self.curve_angle = curve_angle
-        self.attach_len = attach_len
+        self.bend_mag = bend_mag
+        self.bend_angle = bend_angle
+        self.attach_depth = attach_depth
+        self.attach_mag = attach_mag
         self.attach_angle = attach_angle
         #
         self.handle_len = handle_len
@@ -44,59 +46,101 @@ def couple_str(v):
 
 
 # Create a new SVG drawing
-DWG = svgwrite.Drawing("bow.svg", size=('1800px', '600px'))
+DWG = svgwrite.Drawing("bow.svg", size=('600px', '600px'))
 
 def draw_bow(bow, offset=Vector2(0,0)):
-    # calculate control points
-    top_curve = BASEXY + Vector2(bow.push, -bow.height/2) + offset
-    top_wing = move_vector(top_end, 90+bow.wing_angle, bow.wing_len)
-    top_attach = move_vector(top_end, 90+bow.angle, bow.bend_len)
-    top_curve_control = move_vector(top_end, 90+bow.angle, bow.wing_len)
-    top_wing_control = move_vector(top_end, 90+bow.angle, bow.wing_len)
-    top_attach_control = move_vector(top_bend, 90+bow.angle, bow.curve_len*2)
+    # note: we will draw from attach out to the tip
     #
-    bottom_end = BASEXY + Vector2(bow.push, bow.height/2) + offset
-    bottom_bend = move_vector(bottom_end, -90+bow.angle, bow.bend_len)
-    bottom_control = move_vector(bottom_bend, -90+bow.angle, bow.curve_len/2)
+    wing_ratio = bow.wing_len*4 / bow.height
+    #
+    # calculate control points
+    top_bend = BASEXY + Vector2(bow.push, -bow.height/2) + offset
+    top_tip = move_vector(top_bend, -90+bow.wing_angle, bow.wing_len)
+    top_attach = BASEXY + Vector2(bow.attach_depth, -bow.handle_len) + offset
+    top_attach_control = move_vector(top_attach, -90+bow.attach_angle, bow.attach_mag)
+    top_bend_control = move_vector(top_bend, 90-bow.bend_angle, bow.bend_mag)
+    top_wing_control = move_vector(top_bend, 270-bow.bend_angle, bow.bend_mag*wing_ratio)
+    #
+    bottom_bend = BASEXY + Vector2(bow.push, bow.height/2) + offset
+    bottom_tip = move_vector(bottom_bend, 90-bow.wing_angle, bow.wing_len)
+    bottom_attach = BASEXY + Vector2(bow.attach_depth, + bow.handle_len) + offset
+    bottom_attach_control = move_vector(bottom_attach, 90-bow.attach_angle, bow.attach_mag)
+    bottom_bend_control = move_vector(bottom_bend, -90+bow.bend_angle, bow.bend_mag)
+    bottom_wing_control = move_vector(bottom_bend, -270+bow.bend_angle, bow.bend_mag*wing_ratio)
     #
     string_mid = BASEXY + Vector2(-bow.pull, 0) + offset
     
     # Draw bow string
-    path_str = ("M " + couple_str(top_end) +
+    string_path_str = ("M " + couple_str(top_bend) +
                 " L " + couple_str(string_mid) +
-                " L " + couple_str(bottom_end)
+                " L " + couple_str(bottom_bend)
                 )
-    bow_string = DWG.path(d=path_str,
+    bow_string = DWG.path(d=string_path_str,
                           stroke="rgb(0, 0, 0)",
                           fill="none",
                           stroke_width=LINE_THICKNESS,
                           stroke_linejoin='round')
     DWG.add(bow_string)
-    
+
     # Draw bow curve
-    path_str = ( "M " + couple_str(top_end) +
-                " L " + couple_str(top_bend) +
-                " C " + couple_str(top_control) + (
-                    " " + couple_str(bottom_control) + " " + couple_str(bottom_bend)) +
-                " L " + couple_str(bottom_end)
-                )
+    top_path_str = ( "M " + couple_str(bottom_attach) +
+                     "L " + couple_str(top_attach) +
+                     " C " + couple_str(top_attach_control) + (
+                         " " + couple_str(top_bend_control) + " " + couple_str(top_bend)) +
+                     " C " + couple_str(top_wing_control) + (
+                         " " + couple_str(top_tip) + " " + couple_str(top_tip)) 
+                    )
+    bottom_path_str = ( "M " + couple_str(top_attach) +
+                        "L " + couple_str(bottom_attach) +
+                     " C " + couple_str(bottom_attach_control) + (
+                         " " + couple_str(bottom_bend_control) + " " + couple_str(bottom_bend)) +
+                     " C " + couple_str(bottom_wing_control) + (
+                         " " + couple_str(bottom_tip) + " " + couple_str(bottom_tip)) 
+                    )
     
-    bow_outline = DWG.path(d=path_str,
+    top_outline = DWG.path(d=top_path_str,
                         stroke="rgb(0, 0, 0)",
                         fill="none",
                         stroke_width=bow.thickness+LINE_THICKNESS*2, 
                         stroke_linecap='round')
-    bow_main = DWG.path(d=path_str,
+    bottom_outline = DWG.path(d=bottom_path_str,
+                        stroke="rgb(0, 0, 0)",
+                        fill="none",
+                        stroke_width=bow.thickness+LINE_THICKNESS*2, 
+                        stroke_linecap='round')
+
+    top_staff = DWG.path(d=top_path_str,
                         stroke="rgb(220, 130, 75)",
                         fill="none",
                         stroke_width=bow.thickness,
                         stroke_linecap='round')
-    DWG.add(bow_outline)
-    DWG.add(bow_main)
+    bottom_staff = DWG.path(d=bottom_path_str,
+                        stroke="rgb(220, 130, 75)",
+                        fill="none",
+                        stroke_width=bow.thickness,
+                        stroke_linecap='round')
+    DWG.add(top_outline)
+    DWG.add(bottom_outline)
+    DWG.add(top_staff)
+    DWG.add(bottom_staff)
 
+    # Draw handle
+    handle_path_str = ("M " + couple_str(top_attach) +
+                       " L " + couple_str(bottom_attach)
+                       )
+    handle = DWG.path(d=handle_path_str,
+                      stroke="rgb(120, 100, 100)",
+                      fill="none",
+                      stroke_width=bow.thickness)
+    DWG.add(handle)
+    
+    
 
 def pushed_bow(b):
-    return BowDef(b.angle+3, b.bend_len, b.curve_len+7, thickness=b.thickness,
+    return RecurveDef(wing_len, wing_angle, bend_mag, bend_angle,
+                 attach_depth, attach_mag, attach_angle, handle_len=30,
+                      handle_dist=100, height=b.height-15, push=b.ush+25, pull=b.pull+25):
+    b.angle+3, b.bend_len, b.curve_len+7, thickness=b.thickness,
                   height=b.height-15, push=b.push+25, pull=b.pull+25)
 
 def p_pushed_bow(b, p):
@@ -105,19 +149,19 @@ def p_pushed_bow(b, p):
 
 
 # draw dat
-bow_a = BowDef(70, 10, 150, height=420, thickness=40)
-bow_b = p_pushed_bow(bow_a, 30)
-bow_c = p_pushed_bow(bow_a, 55)
+bow_a = RecurveDef(wing_len=20, wing_angle=20, bend_mag=70, bend_angle=0,
+                   attach_depth=110, attach_mag=170, attach_angle=0,
+                   handle_len=0, height=430)
 
-draw_bow(bow_a, Vector2(100, 0))
-draw_bow(bow_b, Vector2(700, 0))
-draw_bow(bow_c, Vector2(1300, 0))
+draw_bow(bow_a, Vector2(0, 0))
+
 
 # Save the drawing
+name = "Short_Bow_I"
 DWG.save()
 cairosvg.svg2png(url='./bow.svg', write_to='./temp.png')
 
 image = Image.open('./temp.png')
 blurred = image.filter(ImageFilter.GaussianBlur(2))
 
-blurred.save('./bow.png')
+blurred.save('./' + name + '.png')
